cce514c5abfa1c62f376c71356ae559490d8fcc9
Vitaliy <donciclukamavs@gmail.com>
2024-01-15 16:03:43 +0100

    Update RedirectPage.tsx

--- src/pages/RedirectPage.tsx
+++ src/pages/RedirectPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom";
 import { addCredentials, addScan, getUser } from "../utils";
 import { useMyContext } from "../providers/ContextProvider";
