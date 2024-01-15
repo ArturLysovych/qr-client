fa73999f0e4402ebb62a3483f2f2a5b1059df235
Vitaliy <donciclukamavs@gmail.com>
2024-01-15 03:41:46 +0100

    environment setup commit

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export const createUser = async (id: string): Promise<any> => {
-	const res = await axios.post('http://localhost:5000/users', { id: id })
+	const res = await axios.post(`${import.meta.env.VITE_APP_SERVER_URL}/users`, { id: id })
 	return res.data;
 }
 
 export const addScan = async (id: string): Promise<any> => {
-	const res = await axios.put(`http://localhost:5000/users/${id}`, { date: new Date() })
+	const res = await axios.put(`${import.meta.env.VITE_APP_SERVER_URL}/users/${id}`, { date: new Date() })
 	return res.data;
 }
 
 export const getScansValue = async (): Promise<string[]> => {
-	const res = await axios.get(`http://localhost:5000/users/scans`)
+	const res = await axios.get(`${import.meta.env.VITE_APP_SERVER_URL}/users/scans`)
 	return res.data.toString().split("")
 }
 
 export const fetchData = async () => {
-	const response = await axios.get('http://localhost:5000/users');
+	const response = await axios.get(`${import.meta.env.VITE_APP_SERVER_URL}/users`);
 	const sortedData = response.data.sort((a: IUser, b: IUser) => b.timesScanned - a.timesScanned);
 	return sortedData;
 }
