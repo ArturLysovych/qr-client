120e1ead0599a784c025d353ef80f4a4b66f9f51
Vitaliy <salinskiyvitaliy@gmail.com>
2024-01-18 15:35:53 +0100

    branches merging commit

--- src/pages/QrPage.tsx
+++ src/pages/QrPage.tsx
@@ -51,16 +51,16 @@ function QrPage() {
 
 	return (
 		<div className="bg-red-500 relative">
-			<div className="container mx-auto px-[20px] max-w-screen-lg		">
+			<div className="container mx-auto px-[20px] max-w-screen-lg">
 				<div className="min-h-screen flex flex-col items-center py-16 gap-10 text-white font-bold">
 
 					<h1 className="text-[50px] font-bold text-center leading-[110%] w-full bg-white text-red-500 absolute py-[20px]">ScPoints Farmer</h1>
-					
+
 					<div className="text-[50px] text-transparent select-none">
 						empty
 					</div>
 
-					<div className="flex gap-10 flex-col w-full items-center mt-[20px] lg:flex-row lg:items-center lg:justify-between">
+					<div className="flex gap-10 flex-col w-full items-center mt-[40px] lg:flex-row lg:items-center lg:justify-between">
 						<div className="flex flex-col justify-center items-center gap-2">
 							<h3 className="text-[30px] text-center max-w-[225px]">Scan this code to get a point</h3>
 							<div className="h-[310px] w-[310px] lg:h-[350px] lg:w-[350px] bg-white rounded-xl flex justify-center items-center border-[4px] border-gray-300">
@@ -79,14 +79,14 @@ function QrPage() {
 							<div className="flex flex-col gap-[10px]">
 								<div className="gap-3 flex justify-center">
 									{scans.map((item, index) => (
-										<div key={`${index}-${item}`} className="text-[72px] xl:text-[110px] h-[90px] xl:h-[140px] w-[90px] xl:w-[140px] bg-[#A55205] flex justify-center items-center rounded-xl">
+										<div key={`${index}-${item}`} className="text-[72px] xl:text-[110px] h-[90px] xl:h-[140px] w-[90px] xl:w-[140px] bg-[#a50d05] flex justify-center items-center rounded-xl">
 											<p>{item}</p>
 										</div>
 									))}
 								</div>
-								<h4 className="text-[32px] text-center">You ScPoints</h4>
+								<h4 className="text-[32px] text-center">Your ScPoints</h4>
 							</div>
-							<div className="flex justify-center w-[310px] sm:w-[340px]">
+							<div className="flex justify-center mx-auto items-center w-[310px] sm:w-[340px]">
 								<LinkButton to="/users">See users</LinkButton>
 							</div>
 						</div>
