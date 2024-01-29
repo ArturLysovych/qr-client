e697f757d998c5617eed8146bf46883951a6a681
ArturLysovych <arturlisovic@gmail.com>
2024-01-29 22:52:34 +0200

    Product purchase approval has been implemented

--- src/components/GoodItem.tsx
+++ src/components/GoodItem.tsx
@@ -1,55 +1,29 @@
-import React from 'react';
-
-import { createRequest } from '../utils';
 import { IProduct } from '../interfaces';
-import { useMyContext } from '../providers/ContextProvider';
-
 const baseUlr = import.meta.env.VITE_APP_SERVER_URL;
+import { Dispatch } from 'react';
 
 interface GoodItemProps {
-	product: IProduct;
-	userId: string | null;
-	setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
-
+    product: IProduct;
+    setCurrItem: Dispatch<IProduct | null>;
 }
 
-export const GoodItem = ({ product, userId, setIsLoading }: GoodItemProps) => {
-
-	const { setResponse } = useMyContext();
-
-	const handleRequestSend = async (userId: string | null, productId: string) => {
-		if (userId) {
-			setIsLoading(true)
-			const response = await createRequest(userId, productId)
-				.then(() => {
-					setResponse(null)
-					setResponse("Request has been successfully send")
-				})
-				.catch(() => {
-					setResponse(null)
-					setResponse("You do not have enough points")
-				})
-				.finally(() => setIsLoading(false))
-			return response;
-		}
-		return null;
-	}
-
+export const GoodItem = ({ product, setCurrItem }: GoodItemProps) => {
 	return (
 		<div
-			className="h-[240px] w-[150px] bg-white flex flex-col justify-around items-center rounded-xl border-red-400 transition-all duration-300 border-[1px] cursor-pointer hover:border-[5px]"
-			onClick={() => handleRequestSend(userId, product._id)}
-		>
+            className="h-[240px] w-[150px] bg-white flex flex-col justify-around items-center rounded-xl border-red-400 transition-all duration-300 border-[1px] cursor-pointer hover:border-[5px]"
+            onClick={() => setCurrItem(product)}
+        >
 
 			<div className='w-full h-[130px] px-2 py-0'>
 				<img
 					src={`${baseUlr}/${product.image}`}
 					alt="good img"
-					className='object-cover h-full w-full'
+					className='object-cover h-full w-full rounded-md'
 					onError={(e) => {
 						(e.target as HTMLImageElement).src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEcTP7Xdiuel5OammWjMZJwcerzXu7D0DZaw&usqp=CAU';
 					}}
-				/>			</div>
+				/>
+			</div>
 
 			<h3 className='w-full bg-red-400 text-white text-xl flex justify-center text-center'>{product.name}</h3>
 			<h2 className='text-2xl text-red-500 font-bold'>{product.price} points</h2>
--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 import FingerprintJS from "@fingerprintjs/fingerprintjs"
 import { Link } from 'react-router-dom';
-
+import { createRequest } from '../utils';
 import { useMyContext } from '../providers/ContextProvider';
+
 import { GoodItem } from '../components/GoodItem';
 import Loader from '../components/Loader';
 import Popup from '../components/Popup';
@@ -15,6 +16,8 @@ const ShopPage = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const initialRender = useRef<boolean>(true);
 	const [user, setUser] = useState<IUser>();
+	const { setResponse } = useMyContext();
+	const [currentItem, setCurrentItem] = useState<IProduct | null>();
 
 	const { id, setId, response } = useMyContext();
 
@@ -53,12 +56,34 @@ const ShopPage = () => {
 			.catch(() => { })
 	}
 
+	const handleRequestSend = async (userId: string | null, productId: string) => {
+		if (userId) {
+			try {
+				await createRequest(userId, productId);
+				setResponse("Request has been successfully sent");
+			} catch (error) {
+				setResponse("You do not have enough points");
+				setCurrentItem(null);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+	};
+
+	const handleYesClick = () => {
+		handleRequestSend(user?.id as string, currentItem?._id as string);
+    };
+
+    const handleNoClick = () => {
+        setCurrentItem(null);
+    };
+
 	return (
 		<div className="bg-red-500 pt-5">
 
 			<div className='w-full appContainer flex justify-between items-center'>
 				<Link to='/' className="outline-none text-[14px] font-bold text-center leading-[110%] bg-white text-red-500 p-2 rounded-xl cursor-pointer"> Back to QR-page</Link>
-
+		
 				{user?.name && user?.surname &&
 					<div className='text-white text-lg'>{user?.name} {user?.surname}: {user?.timesScanned} points</div>
 				}
@@ -67,11 +92,27 @@ const ShopPage = () => {
 
 			<h2 className="text-[36px] sm:text-[50px] font-bold text-center leading-[110%] w-full bg-white text-red-500  mt-6 py-[20px]">Shop</h2>
 
-			<div className='appContainer mt-20 mb-10'>
+			{currentItem ? (
+                <div className="w-full h-screen flex items-center justify-center backdrop-blur-sm fixed top-0">
+                    <div className="max-w-[300px] bg-white shadow-sm shadow-white rounded-lg flex flex-col justify-around items-center gap-[20px] p-[20px]">
+                        <h2 className='max-w-[280px] text-center whitespace-normal'>Are you sure to buy <span className='text-red-500'>{currentItem.name}</span>?</h2>
+                        <div className='flex gap-[20px]'>
+                            <button className='w-[70px] h-[30px] border-2 border-red-500 bg-red-500 hover:bg-transparent outline-none text-white hover:text-red-500 rounded-md transition-colors disabled:opacity-75'
+                                onClick={handleYesClick}
+                            >Yes</button>
+                            <button className='w-[70px] h-[30px] border-2 border-red-600 bg-red-600 hover:bg-transparent outline-none text-white hover:text-red-600 rounded-md transition-colors disabled:opacity-75'
+                                onClick={handleNoClick}
+                            >No</button>
+                        </div>
+                    </div>
+                </div>
+            ): null}
+			
 
+			<div className='appContainer mt-20 mb-10'>
 				<div className="flex flex-wrap justify-around items-center gap-[20px] md:gap-[30px]">
 					{products && products.length !== 0 ? products.map((item: IProduct, index: number) => (
-						<GoodItem key={index} product={item} userId={id} setIsLoading={setIsLoading} />
+						<GoodItem key={index} product={item} setCurrItem={setCurrentItem} />
 					)) : (
 						<h2 className='text-[34px] text-white font-bold'>There are no products so far</h2>
 					)}
